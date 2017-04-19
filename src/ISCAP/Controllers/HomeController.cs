60c7041f9f2eda84107570460adfe38aca6ff802
cc951873 <cc951873@CC205-27>
2017-04-19 17:29:41 -0500

    I inserted a partial view of the schedule into the home view.

--- src/ISCAP/Controllers/HomeController.cs
+++ src/ISCAP/Controllers/HomeController.cs
@@ -3,14 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ISCAP.ViewModel;
+using ISCAP.Models;
+using ISCAP.Data;
+
 
 namespace ISCAP.Controllers
 {
+
     public class HomeController : Controller
     {
+        public ApplicationDbContext db;
+
+        public HomeController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet, Route("Form")]
+        public ViewResult Form()
+        {
+            WriteConferenceScheduleViewModel cs = new WriteConferenceScheduleViewModel();
+            cs.Event = new Event();
+            return View(cs);
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Slot> Slot;
+            ReadConferenceScheduleViewModel cs = new ReadConferenceScheduleViewModel();
+
+            cs.Event = db.Event.ToList();
+            cs.Room = db.Room.ToList();
+
+            Slot = db.Slot.ToList();
+
+            for (var i = 0; i > cs.Room.Count; i++)
+            {
+                for (var x = 0; x > Slot.Count; x++)
+                {
+                    if (cs.Room[i].roomId == Slot[x].roomId)
+                    {
+                        cs.Room[i].Slot.Add(Slot[x]);
+                    }
+                }
+            }
+            return View(cs);
         }
 
         public IActionResult About()
